906f80879b9b709e9795877913217e756b375854
Davinder Kumar <davinderkumar8166@gmail.com>
2020-02-18 23:48:49 +0530

    Greeter Startup

---
+++ Greetr.js
@@ -0,0 +1,9 @@
+(function(global, $) {
+    var Greeter = function(firstname, lastname, language) {
+        return new Greeter.init(firstname, lastname, language)
+    }
+
+    Greeter.init = function(firstname, lastname, language) {
+
+    }
+}(window, jQuery))
